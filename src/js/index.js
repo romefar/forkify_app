d61938177fcb7b5dabcf1ecae3c258e047b68b06
romefar <vlad.romanovr@gmail.com>
2019-03-27 18:09:42 +0300

    added recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search'
+import Recipe from "./models/Recipe"
 import * as searchView from './views/searchView';
 import { DOMelements, renderLoader, clearLoader} from './views/base'
 // Global state (data) of our app
@@ -7,6 +8,11 @@ import { DOMelements, renderLoader, clearLoader} from './views/base'
 // - shopping list object
 // - Liked recipes 
 const state = {};
+
+
+/*
+    SEARCH CONTROLLER
+*/
 
 const controlSearch = async () => { 
     // 1. Get query from view
@@ -20,15 +26,19 @@ const controlSearch = async () => {
         searchView.clearInputField();
         searchView.clearResults();
         renderLoader(DOMelements.searchResults);
-
-        // 4. Search for recipes
-        await state.search.getDataFromAPI();
-
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.recipes);
-
-        console.log(state.search.recipes);
+      
+        try {
+            // 4. Search for recipes
+            await state.search.getDataFromAPI();
+             
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.recipes);
+            console.log(state.search.recipes);
+        } catch (error) {
+            console.log(`Error. Something went wrong. Message: ${error}`);
+            clearLoader();
+        }       
     }
 }
 
@@ -37,5 +47,58 @@ DOMelements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
-// const search = new Search('pizza').getDataFromAPI();
-// console.log(search);
+DOMelements.resultsPages.addEventListener('click', (e) => { 
+    const button = e.target.closest('.btn-inline');
+    if(button) { 
+        const gotoPageData = parseInt(button.dataset.goto);
+        searchView.clearResults();
+        searchView.renderResults(state.search.recipes, gotoPageData);
+    }
+});
+
+
+/*
+    RECIPE CONTROLLER
+*/
+
+const controlRecipe = async () => { 
+    // 1. Get id(hash) from the URL
+    const id = window.location.hash.slice(1);
+
+    // if id exist 
+    if(id) { 
+
+        // 2. Create new Recipe object and add to state
+        state.recipe = new Recipe(id);
+
+        // 3. Prepare UI for result
+        
+        //renderLoader(DOMelements.searchResults);
+
+        // 4. Get recipe data
+        try {
+            await state.recipe.getRecipe();
+            console.log(state.recipe.ingredients);
+            // parse the ingredients
+            state.recipe.parseIngredients();
+             // 5. Calc servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+ 
+
+        } catch (error) {
+            console.log(`Error. Something went wrong. Message: ${error}`);
+        }    
+
+       
+        // 6. Render results on UI
+        //clearLoader();
+        //searchView.renderResults(state.search.recipes);
+
+        console.log(state.recipe);
+    }
+}
+
+// hashchange event fired every time, when the URL's hash has changed
+window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
