aaf285e2ca9b8a3f89ef10625f9833ca5396360b
romefar <vlad.romanovr@gmail.com>
2019-03-31 17:54:43 +0300

    finished likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,30 @@
+export default class Likes { 
+    constructor() { 
+        this.likes = [];
+    }
+
+    addItem(id, title, author, img) { 
+        const like = { 
+            id,
+            title,
+            author,
+            img
+        };
+
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteItem(id) { 
+        const index = this.likes.findIndex(item => item.id === id);
+        this.likes.splice(index, 1);
+    }
+
+    isLiked(id) {
+        return this.likes.findIndex(item => item.id === id) !== -1;
+    }
+
+    getNumberOfLikes() { 
+        return this.likes.length;
+    }
+}
