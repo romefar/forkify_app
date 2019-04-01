d261dfc301773c36c6065a1912e7b65050de72bd
romefar <vlad.romanovr@gmail.com>
2019-04-01 15:25:08 +0300

    added localStorage support

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -12,12 +12,18 @@ export default class Likes {
         };
 
         this.likes.push(like);
+        
+        // add data to localStorage
+        this.preserveData();
         return like;
     }
 
     deleteItem(id) { 
         const index = this.likes.findIndex(item => item.id === id);
         this.likes.splice(index, 1);
+
+        // add data to localStorage
+        this.preserveData();
     }
 
     isLiked(id) {
@@ -26,5 +32,18 @@ export default class Likes {
 
     getNumberOfLikes() { 
         return this.likes.length;
+    }
+
+    // method to keep likes data in browser
+    preserveData() { 
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    // method to retrieve data
+    readDataFromStorage() { 
+        const likesObj = JSON.parse(localStorage.getItem('likes'));
+        if(likesObj) { 
+            this.likes = likesObj;
+        }
     }
 }
